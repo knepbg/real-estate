d8653410462e24e2aeafe2874110a82195fb2b0c
knepbg <knep.87@gmail.com>
2022-01-06 13:44:47 +0200

    my first sourcetree commit

--- src/main/java/com/template/demo/service/NeighbourhoodService.java
+++ src/main/java/com/template/demo/service/NeighbourhoodService.java
@@ -1,6 +1,22 @@
 package com.template.demo.service;
 
+import com.template.demo.model.Floor;
+import com.template.demo.model.Neighbourhood;
+
+import java.util.Set;
+
 public interface NeighbourhoodService {
 
+    Neighbourhood save (Neighbourhood neighbourhood);
+
+    Neighbourhood findByName(String name);
+
+    Neighbourhood findById(Long id);
+
+    Neighbourhood update (Neighbourhood neighbourhood, Long id);
+
+    void delete(Long id);
+
+    Set<Neighbourhood> findAll();
 
 }
--- src/main/java/com/template/demo/service/impl/NeighbourhoodServiceImpl.java
+++ src/main/java/com/template/demo/service/impl/NeighbourhoodServiceImpl.java
@@ -1,9 +1,12 @@
 package com.template.demo.service.impl;
 
+import com.template.demo.model.Neighbourhood;
 import com.template.demo.repository.NeighbourhoodRepository;
 import com.template.demo.service.NeighbourhoodService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 public class NeighbourhoodServiceImpl implements NeighbourhoodService {
@@ -13,5 +16,35 @@ public class NeighbourhoodServiceImpl implements NeighbourhoodService {
     @Autowired
     public NeighbourhoodServiceImpl (NeighbourhoodRepository neighbourhoodRepository) {
         this.neighbourhoodRepository=neighbourhoodRepository;
+    }
+
+    @Override
+    public Neighbourhood save(Neighbourhood neighbourhood) {
+        return neighbourhoodRepository.save(neighbourhood);
+    }
+
+    @Override
+    public Neighbourhood findByName(String name) {
+        return null;
+    }
+
+    @Override
+    public Neighbourhood findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Neighbourhood update(Neighbourhood neighbourhood, Long id) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+
+    @Override
+    public Set<Neighbourhood> findAll() {
+        return null;
     }
 }
